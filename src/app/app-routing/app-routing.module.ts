7a07002a9a59568dc3f3f5ebe191188e6502be8e
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2019-06-02 20:36:35 +0700

    save

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { SoccerComponent } from '../soccer/soccer.component';
+import { SoccerDetailsComponent } from '../soccer-details/soccer-details.component';
+import { AddSoccerComponent } from '../add-soccer/add-soccer.component';
+const routes: Routes = [
+  {
+    path: 'soccers',
+    component: SoccerComponent
+  },
+  {
+    path: 'soccer/add',
+    component: AddSoccerComponent
+  },
+  {
+    path: 'soccers/:id',
+    component: SoccerDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'soccers',
+    pathMatch: 'full'
+  },
+];
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
